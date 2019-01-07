9a933794a17476d3ba8dace147f848a96316e4f2
Natesh Narain <nnaraindev@gmail.com>
2019-01-07 17:53:00 -0500

    Attempt to load binary file

--- src/gameboycore_web.cpp
+++ src/gameboycore_web.cpp
@@ -2,6 +2,9 @@
 #include <gameboycore/gameboycore.h>
 
 #include <memory>
+#include <string>
+#include <sstream>
+#include <iomanip>
 
 using namespace emscripten;
 using namespace gb;
@@ -16,13 +19,47 @@ namespace
         {
         }
 
+        std::string loadROM(const uintptr_t handle, size_t length)
+        {
+            try
+            {
+                const auto buffer = reinterpret_cast<const uint8_t*>(handle);
+                core_->loadROM(buffer, length);
+            }
+            catch(const std::runtime_error& e)
+            {
+                const std::string message{e.what()};
+                return message;
+            }
+
+            return std::string{"Success"};
+        }
+
+        // std::string loadROM(const std::string buffer)
+        // {
+        //     try
+        //     {
+        //         core_->loadROM(reinterpret_cast<const uint8_t*>(buffer.data()), buffer.size());
+        //     }
+        //     catch(const std::runtime_error& e)
+        //     {
+        //         const std::string message{e.what()};
+        //         return message;
+        //     }
+
+        //     return std::string{"Success"};
+        // }
+
+        /**
+         * This must be explicitly called as emscripten does not guarantee the class destructor is called.
+        */
         void release()
         {
             core_.reset();
         }
 
     private:
-        std::unique_ptr<GameboyCore> core_;
+        std::unique_ptr<GameboyCore> core_; 
     };
 }
 
@@ -30,6 +67,7 @@ EMSCRIPTEN_BINDINGS(gameboycore)
 {
     class_<GameboyCoreJs>("GameboyCore")
         .constructor<>()
-        .function("release", &GameboyCoreJs::release);
+        .function("release", &GameboyCoreJs::release)
+        .function("loadROM", &GameboyCoreJs::loadROM);
 }
 
