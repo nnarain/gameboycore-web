9e18e80a9a804e896e0fce79e48ffd0ec7028e1e
Natesh Narain <nnaraindev@gmail.com>
2019-01-09 21:06:56 -0500

    Added jquery

--- gameboycore_web/src/components/display/Display.tsx
+++ gameboycore_web/src/components/display/Display.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
 
-import Button from '@material-ui/core/Button';
 
 class Display extends React.Component {
     public render() {
         return (
-            <Button variant="contained" color="primary">
-                Hello World
-            </Button>
+            <canvas id="canvas" width="800" height="600">Canvas not supported</canvas>
         );
     }
 }
