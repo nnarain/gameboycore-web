d3d2c87252803322160928193dce336717ad2af5
Natesh Narain <nnaraindev@gmail.com>
2019-01-09 20:57:30 -0500

    Added react and typescript scaffold

---
+++ gameboycore_web/src/components/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+import Display from './display/Display';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <Display />
+    );
+  }
+}
+
+export default App;
---
+++ gameboycore_web/src/components/display/Display.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import Button from '@material-ui/core/Button';
+
+class Display extends React.Component {
+    public render() {
+        return (
+            <Button variant="contained" color="primary">
+                Hello World
+            </Button>
+        );
+    }
+}
+
+export default Display;
