3af44141fcf048055c5e2753f5354f5772def682
Natesh Narain <nnaraindev@gmail.com>
2019-01-13 23:13:38 -0500

    Load and initilize gameboycore

--- gameboycore_web/src/components/App.tsx
+++ gameboycore_web/src/components/App.tsx
@@ -1,14 +1,51 @@
 import * as React from 'react';
 import './App.css';
 
-import Display from './display/Display';
+import gameboy_wasm from 'gameboycore/dist/gameboycore.wasm';
 
-class App extends React.Component {
-  public render() {
-    return (
-      <Display />
-    );
-  }
+import {GameboyCoreJS} from 'gameboycore';
+
+interface IAppState {
+    results: number;
+    core: GameboyCoreJS["GameboyCore"];
+    module: any;
+}
+
+class App extends React.Component<{}, IAppState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {results: 0, core: null, module: null};
+    }
+    public render() {
+        return (
+            <p>{this.state.results}</p>
+        );
+    }
+
+    public componentDidMount() {
+        import('gameboycore').then(gb => {
+            if (gb != null) {
+                this.setState({results: 1});
+                gb.default({
+                    locateFile: (filename: string, dir: string): string => {
+                        if (filename === 'gameboycore.wasm') {
+                            return gameboy_wasm;
+                        }
+                        else {
+                            return "";
+                        }
+                    },
+                    onRuntimeInitialized: () => {
+                        console.log('GameboyCoreJS Initialized');
+                    }
+                })
+            }
+            else {
+                this.setState({results: 2});
+            }
+        });
+    }
+
 }
 
 export default App;
--- gameboycore_web/src/components/display/Display.tsx
+++ gameboycore_web/src/components/display/Display.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 class Display extends React.Component {
     public render() {
         return (
-            <canvas id="canvas" width="800" height="600">Canvas not supported</canvas>
+            <p>Hello World</p>
         );
     }
 }
